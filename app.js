832ae6c5b13b0a5b9c1cb56b9ad5d052e7842485
Christian Alexander <christian.alexanderato@gmail.com>
2023-11-09 20:46:31 -0800

    debugged - user cannot click  button when game starts

--- app.js
+++ app.js
@@ -2,19 +2,18 @@
 let money = 0;
 let accountBal = [];
 let newTool = "";
-let tools = [
-  { type: "teeth", pay: 1 },
-  { type: "Rusty Scissors", pay: 5 },
-];
+let tools = [{ type: "teeth" }, { type: "rusty scissors" }];
 // cut with teeth function
 function teethCut() {
   if (money < 5) {
     accountBal.splice(0, 1, (money += 1));
-    console.log(`You have earned ${money} cutting lawns today!`)
+    console.log(`You have earned ${money} cutting lawns today!`);
     // alert(`You have earned ${money} cutting lawns today!`);
   }
   if (money === 5) {
-    console.log(`You have earned ${money} to purchase a tool to make you more productive! Proceed to the store!`)
+    console.log(
+      `You have earned ${money} to purchase a tool to make you more productive! Proceed to the store!`
+    );
     // alert(
     //   `You have earned ${money} to purchase a tool to make you more productive! Proceed to the store!`
     // );
@@ -22,24 +21,30 @@ function teethCut() {
 }
 // cut with scissors function
 function scissCut() {
-  if (money < 25) {
+  if (money < 25 && newTool === 'rusty scissors') {
     accountBal.splice(0, 1, (money += 5));
-    console.log((`You have earned ${money}! Keep up the good work!`))
+    console.log(`You have earned ${money}! Keep up the good work!`);
     // alert(`You have earned ${money}! Keep up the good work!`);
-  }
-  if (money === 25) {
-    console.log(`You have earned ${money}! Proceed to the store for a upgrade!`)
-    // alert(`You have earned ${money}! Proceed to the store for a upgrade!`);
+  } else if (newTool !== "rusty scissors") {
+    console.log("You don't have the tool! Get to chomping!");
+    // alert("You don't have enough!")
+  } else if (money === 25) {
+    console.log(
+      `You have earned ${money}! Proceed to the store for a upgrade!`
+      // alert(`You have earned ${money}! Proceed to the store for a upgrade!`);
+    );
   }
 }
 // buy scissors function
 function buySciss() {
   if (accountBal < 5) {
-    alert(`You only have ${accountBal}, Get back to work!`);
+       console.log(`You only have ${accountBal}, Get back to work!`)
+    // alert(`You only have ${accountBal}, Get back to work!`);
   } else if (accountBal >= 5) {
     document.getElementById("scissors");
     accountBal.splice(0, 1, (money -= 5));
     newTool = tools[1].type;
-    alert(`You got ${tools[1].type}! Now you can make more money!`);
+    console.log(`You got ${tools[1].type}! Now you can make more money!`)
+    // alert(`You got ${tools[1].type}! Now you can make more money!`);
   }
 }
